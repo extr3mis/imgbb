54f69b2dae78efe6882654ae567cc56a39c4a081
extr3mis <37597293+extr3mis@users.noreply.github.com>
2020-06-26 00:37:13 +0530

    Improve code readability

--- imgbb/client.py
+++ imgbb/client.py
@@ -1,32 +1,24 @@
 import asyncio
 import aiohttp
-import base64
+
 URL = 'https://api.imgbb.com/1/upload'
 class Client():
     """The default interface to interact with Imgbb."""
-    def __init__(self,key,session=None):
+    def __init__(self, key, session=None):
         self.key = key
         if session is None:
             self.session = aiohttp.ClientSession()
         else: self.session=session
     async def post(self,filename,name):
         """Post using a filename like 'image.jpg'"""
-        with open(filename,'rb') as f:
-            payload = {
-            "key": self.key,
-            "image": f.read(),
-            "name": name
-            }
+        with open(filename, 'rb') as img:
+            payload = {"key" : self.key, "image" : img.read(), "name" : name}
         async with self.session.post(URL, data=payload) as response:
-            rj = await response.json()
-        return rj
-    async def postbyte(self,fp,name):
+            resp = await response.json()
+        return resp
+    async def postbyte(self, bytesfile, name):
         """Use when you want to upload a `bytes` object."""
-        payload = {
-        "key": self.key,
-        "image": fp,
-        "name": name
-        }
+        payload = {"key" : self.key, "image" : bytesfile, "name" : name}
         async with self.session.post(URL, data=payload) as response:
-            rj = await response.json()
-        return rj
+            resp = await response.json()
+        return resp
